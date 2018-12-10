afed6b57e1aca1fbf686ff160b08cadc0417f4e5
mat4645 <matun4645@gmail.com>
2018-12-10 23:59:29 +0900

    アイコン追加_TabBar追加

--- Flowgement/TopViewController.swift
+++ Flowgement/TopViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TopViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //選択したアイコンの色
+        UITabBar.appearance().tintColor = UIColor(red: 163/255, green: 188/255, blue: 226/255, alpha: 1.0)
+
+        //背景の色
+        UITabBar.appearance().barTintColor = UIColor(red: 66/255, green: 74/255, blue: 93/255, alpha: 1.0)
+  
         // Do any additional setup after loading the view, typically from a nib.
     }
 
